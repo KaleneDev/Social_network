c75a1fa0c9576c6d210d1c0d91e51447ac436997
kaleneDev <apiou.kalene@gmail.com>
2023-05-15 16:58:00 +0200

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import "./style/App.css";
+import Header from "./components/Header";
+function App() {
+    return (
+        <div className="app">
+            <Header></Header>
+        </div>
+    );
+}
+
+export default App;
