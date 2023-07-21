d26b2ec1a33dec6906f6f057897f8df03d0b1ceb
KALÈNE\apiou <apiou.kalene@gmail.com>
2023-07-21 18:44:24 +0200

    add extension import component

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 import { NavLink } from "react-router-dom";
 import { useContext } from "react";
 import { UserIdContext } from "../AppContext";
-import Logout from "../log/Logout";
-import SearchBar from "../log/SearchBar";
+import Logout from "../log/Logout.tsx";
+import SearchBar from "../log/SearchBar.tsx";
 import "../../style/components/Navbar.scss";
 import { SlideInFromTop } from "../../utils/AnimationText";
 function Navbar() {
